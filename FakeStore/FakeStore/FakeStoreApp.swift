6f033b2ee7b6b7a2310feb5f424c92a95a660e71
Chandra <developer.chandrasekhar@gmail.com>
2024-11-10 23:20:22 +0530

    • Added initial xcode project

---
+++ FakeStore/FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by chandra sekhar p on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProductsListView()
+        }
+    }
+}
---
+++ FakeStore/FakeStore/ProductsListView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsListView.swift
+//  FakeStore
+//
+//  Created by chandra sekhar p on 10/11/24.
+//
+
+import SwiftUI
+
+struct ProductsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductsListView()
+}
