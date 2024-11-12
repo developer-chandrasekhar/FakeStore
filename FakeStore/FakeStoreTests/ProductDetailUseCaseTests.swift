aa1aef686ecad352bf82906e8c82a1ccc31f5eae
Chandra <developer.chandrasekhar@gmail.com>
2024-11-13 02:18:08 +0530

    • Added ProductDetailUseCaseTests

---
+++ FakeStore/FakeStoreTests/ProductDetailUseCaseTests.swift
@@ -0,0 +1,48 @@
+//
+//  ProductDetailUseCaseTests.swift
+//  FakeStoreTests
+//
+//  Created by chandra sekhar p on 12/11/24.
+//
+
+import XCTest
+@testable import FakeStore
+@testable import NetworkPackageManager
+
+final class ProductDetailUseCaseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
+
+extension ProductDetailUseCaseTests {
+    func test_getProduct_Success() async {
+        let mockRepository = ProductRepositoryMock_success()
+        let useCase = FetchProductDetailUseCase(productRepository: mockRepository)
+        Task {
+            do {
+                let data = try await useCase.getProduct(byId: 1)
+                XCTAssertNotNil(data)
+            } catch {
+                XCTFail("Should not throw error")
+            }
+        }
+    }
+    
+    func test_getProducts_Fail() async {
+        let mockRepository = ProductRepositoryMock_fail()
+        let useCase = FetchProductDetailUseCase(productRepository: mockRepository)
+        Task {
+            do {
+                let _ = try await useCase.getProduct(byId: 1)
+            } catch {
+                XCTAssertTrue(((error as? ApiError) != nil))
+            }
+        }
+    }
+}
