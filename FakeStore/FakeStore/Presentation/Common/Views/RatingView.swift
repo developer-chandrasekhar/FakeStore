8b5d1c7f792d0ca77b31852c42e5357298a0a121
Chandra <developer.chandrasekhar@gmail.com>
2024-11-12 18:10:07 +0530

    • Rating View

--- FakeStore/FakeStore/Presentation/Common/Views/RatingView.swift
+++ FakeStore/FakeStore/Presentation/Common/Views/RatingView.swift
@@ -7,12 +7,36 @@
 
 import SwiftUI
 
+
 struct RatingView: View {
+    
+    let rating: Double
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "star.fill")
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            
+            Text(String(format: "%.1f", rating))
+                .bold()
+                .minimumScaleFactor(0.5)
+                .frame(maxHeight: .infinity)
+        }
+        .padding(.all, 0)
+        .foregroundColor(rateColor(rate: rating))
+    }
+    
+    private func rateColor(rate: Double) -> Color {
+        switch rate {
+        case 0..<2: return .red
+        case 2..<3.5: return .orange
+        default: return .green
+        }
     }
 }
 
 #Preview {
-    RatingView()
+    RatingView(rating: 3.6)
 }
